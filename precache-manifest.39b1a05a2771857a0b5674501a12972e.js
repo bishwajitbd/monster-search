13bc2decd6efdbd9870bbd91ea63208d14daa727
Bishwajit <bbaruaca@gmail.com>
2020-01-31 00:09:38 -0500

    Updates

---
+++ precache-manifest.39b1a05a2771857a0b5674501a12972e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0029d894a9a2635b71911824ee644d37",
+    "url": "/bishwajitbd/monster-search.git/index.html"
+  },
+  {
+    "revision": "2619923d45ac3f2b30b8",
+    "url": "/bishwajitbd/monster-search.git/static/css/main.f6817437.chunk.css"
+  },
+  {
+    "revision": "9a5db7141359941eba9a",
+    "url": "/bishwajitbd/monster-search.git/static/js/2.7bc5effb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bishwajitbd/monster-search.git/static/js/2.7bc5effb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2619923d45ac3f2b30b8",
+    "url": "/bishwajitbd/monster-search.git/static/js/main.f773c1b0.chunk.js"
+  },
+  {
+    "revision": "b955e3b21d5fb59dc7e2",
+    "url": "/bishwajitbd/monster-search.git/static/js/runtime-main.433dcab5.js"
+  }
+]);
